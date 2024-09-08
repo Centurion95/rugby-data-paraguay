821baebd298d30fe923cb991a8eeb08396539bf8
Rodrigo Centurion <rodrigo@debian.centurion>
2024-09-07 22:51:06 -0400

    se agrega /public/sitemap.xml

--- src/pages/Versiones.js
+++ src/pages/Versiones.js
@@ -15,6 +15,14 @@ const Page = () => {
       <h1>Versiones</h1>
 
       <div className="container-c">
+        <p><u>Versión 07/09/2024 22:42</u></p>
+        <ul>
+          <li>Mejoras en el SEO: se agrega `/public/sitemap.xml`, generado por https://www.mysitemapgenerator.com/</li>
+        </ul>
+      </div>
+      <br />
+
+      <div className="container-c">
         <p><u>Versión 27/08/2024 00:05</u></p>
         <ul>
           <li>Mejoras en el SEO según seobility.net</li>
